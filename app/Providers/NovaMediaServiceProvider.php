3deeeebae4815b7e914acd5b9a755639d26704fa
Jamie Sykes <jaymeh@users.noreply.github.com>
2023-11-21 15:59:09 +0000

    Changes service provider.

--- app/Providers/NovaMediaServiceProvider.php
+++ app/Providers/NovaMediaServiceProvider.php
@@ -31,15 +31,10 @@ class NovaMediaServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
 
         // Register config.
-        $this->publishes([__DIR__ . '/../../config/config.php' => config_path('media'.'.php')], 'config');
+        $this->publishes([__DIR__ . '/../../config/config.php' => config_path('nova-media'.'.php')], 'nova-media-config');
 
-        Blade::directive('media_image', function ($media_id) {
-            // Find media by id
-            $media = \Modules\NovaMedia\app\Models\Media::find($media_id);
-
-            // Return image tag with alt_text and url
-            return "<img src='{$media->url}' alt='{$media->alt_text}' />";
-        });
+        // Register blade directives
+        $this->registerBladeDirectives();
     }
 
     /**
@@ -48,7 +43,7 @@ class NovaMediaServiceProvider extends ServiceProvider
     public function register(): void
     {
         // Bind default values into config.
-        $this->mergeConfigFrom(__DIR__ . '/../../config/config.php', 'media');
+        $this->mergeConfigFrom(__DIR__ . '/../../config/config.php', 'nova-media');
 
         Nova::resources([\Modules\NovaMedia\app\Nova\MediaResource::class]);
     }
@@ -59,5 +54,19 @@ class NovaMediaServiceProvider extends ServiceProvider
     public function provides(): array
     {
         return [];
+    }
+
+    /**
+     * Register blade directives.
+     */
+    private function registerBladeDirectives(): void
+    {
+        Blade::directive('mediaImage', function ($media_id) {
+            // Find media by id
+            $media = \Modules\NovaMedia\app\Models\Media::find($media_id);
+
+            // Return image tag with alt_text and url
+            return "<img src='{$media->url}' alt='{$media->alt_text}' />";
+        });
     }
 }
